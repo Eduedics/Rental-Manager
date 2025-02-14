cb63be8b65735bdeba99c119659f7b2dddd6de77
Edward <kemuthii@gmail.com>
2025-02-14 13:06:23 +0300

    stylling estate and block page

--- src/Data.js
+++ src/Data.js
@@ -19,6 +19,20 @@ const estatesData = [
                         ]
                     }
                 ]
+            },
+            {
+                id: "A",
+                name: "Block A",
+                floors: [
+                    {
+                        id: "1",
+                        name: "Floor 1",
+                        rooms: [
+                            { id: "101", number: "1", description: "1 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $500",images:['iphoto.jpg','3photo.jpg'] },
+                            { id: "102", number: "2", description: "2 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $700",images:['iphoto.jpg','3photo.jpg'] }
+                        ]
+                    }
+                ]
             }
         ]
     },
@@ -42,7 +56,79 @@ const estatesData = [
                         ]
                     }
                 ]
-            }
+            },
+            
+        ]
+    },{
+        id: "1",
+        name: "Estate A",
+        location: "City Center",
+        image: "/images/estate-a.jpg",
+        description: "A luxurious estate...",
+        blocks: [
+            {
+                id: "A",
+                name: "Block A",
+                floors: [
+                    {
+                        id: "1",
+                        name: "Floor 1",
+                        rooms: [
+                            { id: "101", number: "1", description: "1 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $500" },
+                            { id: "102", number: "2", description: "2 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $700" }
+                        ]
+                    }
+                ]
+            },
+            
+        ]
+    },
+    {
+        id: "1",
+        name: "Estate A",
+        location: "City Center",
+        image: "/images/estate-a.jpg",
+        description: "A luxurious estate...",
+        blocks: [
+            {
+                id: "A",
+                name: "Block A",
+                floors: [
+                    {
+                        id: "1",
+                        name: "Floor 1",
+                        rooms: [
+                            { id: "101", number: "1", description: "1 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $500" },
+                            { id: "102", number: "2", description: "2 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $700" }
+                        ]
+                    }
+                ]
+            },
+            
+        ]
+    },
+    {
+        id: "1",
+        name: "Estate A",
+        location: "City Center",
+        image: "/images/estate-a.jpg",
+        description: "A luxurious estate...",
+        blocks: [
+            {
+                id: "A",
+                name: "Block A",
+                floors: [
+                    {
+                        id: "1",
+                        name: "Floor 1",
+                        rooms: [
+                            { id: "101", number: "1", description: "1 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $500" },
+                            { id: "102", number: "2", description: "2 Bedroom", amenities: ["AC", "Hot Shower"], booking: "Monthly rent $700" }
+                        ]
+                    }
+                ]
+            },
+            
         ]
     }
 ];
--- src/components/EstateListing.jsx
+++ src/components/EstateListing.jsx
@@ -1,3 +1,4 @@
+import img from '../assets/mediamodifier-8pc4Z3XEBO8-unsplash.jpg'
 import './EstateListingStyles.css'
 // import estate data
 import estateData from "../Data.js"
@@ -21,17 +22,19 @@ export default function EstateListing(){
                 </aside>
                 <aside className='estates'>
                     <h2>Available Estates</h2>
-                    {estateData.map(
-                        estate=>(
-                            <div key={estate.id}>
-                                <h1>{estate.name}</h1>
-                                <img src={estate.img} alt='estate_img'/>
-                                <h3>{estate.location}</h3>
-                                <p className='description'>{estate.description}</p>
-                                <Link to={`/estate/${estate.id}`}>View Estate</Link>
-                            </div>
-                        )
-                    )}
+                    <div className='estate-listing'>
+                        {estateData.map(
+                            estate=>(
+                                <div className='estate-item' key={estate.id}>
+                                    <h1>{estate.name}</h1>
+                                    <img className='estate-img' src={img} alt='estate_img'/>
+                                    <h3 className='location'><span>Location</span>:{estate.location}</h3>
+                                    <p className='description'> <span>Description</span>:{estate.description}</p>
+                                    <Link className='link-EST' to={`/estate/${estate.id}`}>View Estate</Link>
+                                </div>
+                            )
+                        )}
+                    </div>
                 </aside>
 
             </section>
